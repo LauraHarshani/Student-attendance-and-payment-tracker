3df2d45201050d742c8a262195ba6a7556a424a5
IT21839160 <it21839160@my.sliit.lk>
2026-08-18 12:48:26 +0530

    add header title

--- frontend/src/pages/Attendance.jsx
+++ frontend/src/pages/Attendance.jsx
@@ -1,3 +1,23 @@
+import { History } from "lucide-react"
+
 export default function Attendance() {
-  return <h2 className="text-2xl font-bold text-gray-800">Student Attendance Management</h2>;
+  return(
+    <div className="space-y-6">
+
+      {/*header title*/}
+      <div className="flex items-center justify-between">
+        <div >
+          <h1 className="text-3xl font-bold text-black">Attendance</h1>
+          <p className="text-gray-600 text-sm">Manage today's student attendance</p>
+        </div>
+        <div>
+          <button type="button" className="flex items-center gap-2 border rounded-md border-blue-600 px-4 py-2 text-sm font-medium text-blue-600 cursor-pointer transition hover:bg-blue-50"
+          >
+            <History size={18} />
+            History
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }

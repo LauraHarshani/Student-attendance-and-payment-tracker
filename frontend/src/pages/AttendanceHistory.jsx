752709013fe30b115ef2a7b053dec293b32f7d9d
IT21839160 <it21839160@my.sliit.lk>
2026-08-19 18:48:57 +0530

    add header

--- frontend/src/pages/AttendanceHistory.jsx
+++ frontend/src/pages/AttendanceHistory.jsx
@@ -1,6 +1,29 @@
-export default function AttendanceHistory(){
+import { useNavigate } from 'react-router-dom';
+import {
+  ArrowLeft,
+} from 'lucide-react';
 
-    return(
-    <h1>Attendance History</h1>
-)
+export default function AttendanceHistory() {
+
+    const navigate = useNavigate();
+
+    return (
+        <div className='space-y-8'>
+            {/*Header*/}
+            <div>
+                <button
+                onClick={() => navigate('/attendance')}
+                className="mb-4 text-gray-900 hover:text-blue-600 transition-colors"
+                >
+                    <ArrowLeft size={28} />
+                </button>
+
+                <h1 className="text-3xl font-bold text-black">
+                    Attendance History
+                </h1>
+            </div>
+
+           
+        </div>
+    )
 }

4ff4065d9b02c3070eee5b1a87c21e71bd6193df
IT21839160 <it21839160@my.sliit.lk>
2026-08-21 23:13:35 +0530

    add payment history route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import AttendanceHistory from './pages/AttendanceHistory';
 import Payments from './pages/Payments';
 import AdminProfile from './pages/AdminProfile';
 import Login from './pages/Login';
+import PaymentHistory from './pages/PaymentHistory';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="attendance/history" element={<AttendanceHistory/>}/>
           <Route path="payments" element={<Payments />} />
           <Route path="admin-profile" element={<AdminProfile />} />
+          <Route path="payment/history" element={<PaymentHistory />} />
         </Route>
       </Routes>
     </Router>

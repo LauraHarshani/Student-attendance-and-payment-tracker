0d3108eb3899e66cdad6e2ea4eef763d1bf4531f
IT21839160 <it21839160@my.sliit.lk>
2026-08-27 16:54:46 +0530

    attendance model

---
+++ backend/models/Attendance.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const attendanceSchema = new mongoose.Schema({
+
+    date: {
+        type: String,
+        required: true
+    },
+
+    studentId: {
+        type: String,
+        required: true
+    },
+
+    status:{
+        type: String,
+        enum: ["Present", "Absent"],
+        required: true
+    },
+}, {timestamps:true});
+
+module.exports = mongoose.model("Attendance", attendanceSchema);

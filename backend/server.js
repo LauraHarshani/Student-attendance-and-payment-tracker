a134dd671f38b487f683f27016bf8149f02dab2c
IT21839160 <it21839160@my.sliit.lk>
2026-08-28 16:00:06 +0530

    resolve conflict

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,21 @@ dotenv.config();
 // Initialize Express app
 const app = express();
 
-// Middleware (Parse JSON requests and enable CORS)
+// Middleware
 app.use(express.json());
 app.use(cors());
 
 // Import Routes
 const authRoutes = require('./routes/authRoutes');
 const studentRoutes = require('./routes/studentRoutes');
+const AttendanceRoutes = require('./routes/AttendanceRoutes');
+const PaymentRoutes = require('./routes/PaymentRoutes');
 
 // Use Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/students', studentRoutes); // Added Student API routes here
+app.use('/api/students', studentRoutes);
+app.use('/api/attendance', AttendanceRoutes);
+app.use('/api/payments', PaymentRoutes);
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
@@ -35,7 +39,7 @@ app.get('/', (req, res) => {
   res.send('Student Tracker Backend is Running!');
 });
 
-// Get Port from .env or use 5000 as default
+// Get Port
 const PORT = process.env.PORT || 5000;
 
 // Start the server
